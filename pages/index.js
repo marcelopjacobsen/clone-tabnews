812255d45a7aa0e9b34d682daf679e5c8c89df02
marcelopjacobsen <marcelopj182@gmail.com>
2025-11-30 23:51:28 +0000

    mudando a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Só lembrando a galera do grupo, que todos ainda são porcao, parece que os senhores esqueceram!</h1>
+    return <h1>Priemiro Deploy</h1>
 }
 
 export default Home;
